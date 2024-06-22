0d80516d5c5a7c8529b19bceac4e53bb0e2d1a47
pablowinck <contato@pablowinter.com.br>
2024-06-22 11:52:11 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+//! Esse arquivo é só para ter intellicense para extensões do VSCode
+//! Não é utilizado no projeto
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    // Light & dark themes are added by default (it switches automatically based on OS settings)
+    // You can add another theme among the list of 30+
+    // Add "data-theme='theme_name" to any HTML tag to enable the 'theme_name' theme.
+    // https://daisyui.com/
+    themes: ["light", "dark"],
+  },
+};
